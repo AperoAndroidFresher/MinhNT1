a463349af912e4dd669f0434653d727a5200f045
Minh <77561805+mn-0000@users.noreply.github.com>
2025-08-05 22:23:22 +0700

    Chose LoginMviContract.kt as class for Lesson 17 submission. Other classes are affected due to the variable name change.

--- app/src/main/java/com/apero/minhnt1/screens/login/LoginMviContract.kt
+++ app/src/main/java/com/apero/minhnt1/screens/login/LoginMviContract.kt
@@ -9,16 +9,16 @@ data class LoginMviState(
     var username: MutableState<String> = mutableStateOf(""),
     var password: MutableState<String> = mutableStateOf(""),
     var confirmPassword: MutableState<String> = mutableStateOf(""),
-    var email: MutableState<String> = mutableStateOf(""),
+    var emailAddress: MutableState<String> = mutableStateOf(""),
     var selfDescription: MutableState<String> = mutableStateOf(""),
     var loginSuccess: MutableState<Boolean> = mutableStateOf(false),
     var usernameFormatCheck: MutableState<Boolean> = mutableStateOf(true),
     var passwordFormatCheck: MutableState<Boolean> = mutableStateOf(true),
     var confirmPasswordCheck: MutableState<Boolean> = mutableStateOf(true),
     var emailFormatCheck: MutableState<Boolean> = mutableStateOf(true),
-    var isSignupScreen: MutableState<Boolean> =  mutableStateOf(false),
-    var isPasswordVisible: MutableState<Boolean> =mutableStateOf(false)
-    )
+    var isSignupScreen: MutableState<Boolean> = mutableStateOf(false),
+    var isPasswordVisible: MutableState<Boolean> = mutableStateOf(false)
+)
 
 abstract class LoginMviIntents(open val username: String, open val password: String) {
     data class LogIn(override val username: String, override val password: String) :
